85915787ad35dfee1a00ba92c0b2f80ec9a2c7fb
Alan Salazar <alan_salazarb@hotmail.com>
2025-07-30 15:30:59 -0600

    Update app.js

--- app.js
+++ app.js
@@ -5,16 +5,13 @@ let listaAmigosIngresados = [];
 let indiceAleatorio = 0;
 let amigoSorteado = "";
 
-
 //Función que agrega el amigo a la lista y lo muestra en la web
 function agregarAmigo() {
 
     //Si el input no está vacío, agregalo a la lista y muestralo en la pantalla
     if (!(document.getElementById("amigo").value == "")) {
-        //Lógica para capturar el amigo ingreso y agregarlo a la lista
-        añadirAmigoLista();
-        //Agrega el texto de los amigos a la vista
-        mostrarListaAmigos();
+        añadirAmigoLista(); //Lógica para capturar el amigo ingreso y agregarlo a la lista
+        mostrarListaAmigos(); //Agrega el texto de los amigos a la vista
     } else {
         lanzarAlerta("Por favor, ingrese un nombre válido.");
     }
@@ -22,43 +19,31 @@ function agregarAmigo() {
 
 //Función que añade el amigo a la lista
 function añadirAmigoLista(){
-    //Agrega el amigo ingresado al final de la lista
-    listaAmigosIngresados.push(amigoIngresado.value);
-    //Limpia el input del amigo para agregar uno nuevo
-    amigoIngresado.value = "";
-    //Si se vuelve a agregar un amigo, limpia el amigo secreto sorteado inicialmente
-    resultado.innerHTML = "";
+    listaAmigosIngresados.push(amigoIngresado.value); //Agrega el amigo ingresado en el input al final de la lista
+    amigoIngresado.value = ""; //Limpia el input del amigo para agregar uno nuevo
+    resultado.innerHTML = ""; //Si se vuelve a agregar un amigo, limpia el amigo secreto sorteado inicialmente
 }
 
 //Función que selecciona el amigo sorteado del total de la lista
 function sortearAmigo(){
     if (listaAmigosIngresados.length > 0){
         limpiarTextoLista();
-        //Genera un numero aleatorio del total de amigos ingresados
-        indiceAleatorio = Math.floor(Math.random() * listaAmigosIngresados.length);
-        //Del número aleatorio que se generó lo busca en la lista para asignarlo como amigo secreto
-        amigoSorteado = listaAmigosIngresados[indiceAleatorio];
-        //Muestra el resultado
-        resultado.innerHTML = `El amigo secreto sorteado es: ${amigoSorteado}`;
+        indiceAleatorio = Math.floor(Math.random() * listaAmigosIngresados.length); //Genera un numero aleatorio del total de amigos ingresados
+        amigoSorteado = listaAmigosIngresados[indiceAleatorio]; //Del número aleatorio que se generó lo busca en la lista para asignarlo como amigo secreto
+        resultado.innerHTML = `El amigo secreto sorteado es: ${amigoSorteado}`; //Muestra el resultado
     } else {
-        //Si la lista está vacía, no sortea algún amigo para evitar undefined
-        lanzarAlerta("No hay amigos ingresados, ingrese al menos uno.");
+        lanzarAlerta("No hay amigos ingresados, ingrese al menos uno."); //Si la lista está vacía, no sortea algún amigo para evitar undefined
     }
 }
 
 function mostrarListaAmigos(){
-    //Limpia el texto de la lista para que al crear un nuevo amigo no se repita todo nuevamente
     limpiarTextoLista();
     let indicadorLista = 0;
-
     //Mientras el indicadorLista sea menor que el largo de la lista crea un nuevo elemento en el html
     while (indicadorLista < listaAmigosIngresados.length) {
-        // Crea el nuevo elemento que se mostrará
-        let li = document.createElement("li");
-        //Asigna el nuevo amigo al nuevo elemento html
-        li.textContent = listaAmigosIngresados[indicadorLista];
-        //Agrega el elemento li dentro del elemento listaAmigos
-        listaAmigos.appendChild(li);
+        let li = document.createElement("li"); // Crea el nuevo elemento que se mostrará en el html
+        li.textContent = listaAmigosIngresados[indicadorLista]; //Asigna el nuevo amigo al nuevo elemento html
+        listaAmigos.appendChild(li); //Agrega el elemento li dentro del elemento listaAmigos
         indicadorLista++;
     }
 }
